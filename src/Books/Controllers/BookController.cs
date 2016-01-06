33d30db2ff94c31ee9d3962ad3b84539e4126122
Gustavo Garcia <garciagustavoariel@hotmail.com>
2016-01-05 21:26:06 -0300

    initial

---
+++ src/Books/Controllers/BookController.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.AspNet.Mvc;
+using Books.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Data.Entity;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+using Microsoft.AspNet.Mvc.Rendering;
+using Books.Repository;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Books.Controllers
+{
+    public class BookController : Controller
+    {
+
+        [FromServices]
+        public GenericRepository<Book> Books { get; set; }
+
+        [FromServices]
+        public GenericRepository<Author> Authors { get; set; }
+
+        [FromServices]
+        public ILogger<BookController> Logger { get; set; }
+
+        public IActionResult Index()
+        {
+            var books = Books.All().Include(_ => _.Author);
+            return View(books);
+        }
+
+        public async Task<ActionResult> Details(int id)
+        {
+            var book = await Books.All()
+                .Include(b => b.Author)
+                .SingleOrDefaultAsync(b => b.BookID == id);
+            if (book == null)
+            {
+                Logger.LogInformation("Details: Item not found {0}", id);
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.Items = GetAuthorsListItems();
+            return View();
+        }
+
+        private dynamic GetAuthorsListItems()
+        {
+            var tmp = Authors.All().ToList();
+
+            // Create authors list for <select> dropdown Workaround for https://github.com/aspnet/EntityFramework/issues/2246
+            return tmp
+                .OrderBy(author => author.LastName)
+                .Select(author => new SelectListItem
+                {
+                    Text = string.Format("{0}, {1}", author.LastName, author.FirstMidName),
+                    Value = author.AuthorID.ToString()
+                });
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind("Title", "Year", "Price", "Genre", "AuthorID")] Book book)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Books.Add(book);
+                    await Books.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError(string.Empty, "Unable to save changes.");
+            }
+            return View(book);
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            Book book = await FindBookAsync(id);
+            if (book == null)
+            {
+                Logger.LogInformation("Edit: Item not found {0}", id);
+                return HttpNotFound();
+            }
+
+            ViewBag.Items = GetAuthorsListItems(book.AuthorID);
+            return View(book);
+        }
+
+        private dynamic GetAuthorsListItems(int authorID)
+        {
+            var tmp = Authors.All().ToList();  // Workaround for https://github.com/aspnet/EntityFramework/issues/2246
+
+            // Create authors list for <select> dropdown
+            return tmp
+                .OrderBy(author => author.LastName)
+                .Select(author => new SelectListItem
+                {
+                    Text = String.Format("{0}, {1}", author.LastName, author.FirstMidName),
+                    Value = author.AuthorID.ToString(),
+                    Selected = author.AuthorID == authorID
+                });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Update(int id, [Bind("Title", "Year", "Price", "Genre", "AuthorID")] Book book)
+        {
+            try
+            {
+                Book bookhold = await FindBookAsync(id);
+                bookhold.Title = book.Title;
+                bookhold.AuthorID = book.AuthorID;
+                bookhold.Price = book.Price;
+                bookhold.Year = book.Year;
+                bookhold.Genre = book.Genre;
+
+                await Books.SaveChangesAsync();
+      
+                return RedirectToAction("Index");
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError(string.Empty, "Unable to save changes.");
+            }
+            return View(book);
+        }
+
+        private Task<Book> FindBookAsync(int id)
+        {
+            return Books.All().SingleOrDefaultAsync(book => book.BookID == id);
+        }
+
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<ActionResult> ConfirmDelete(int id, bool? retry)
+        {
+            Book book = await FindBookAsync(id);
+            if (book == null)
+            {
+                Logger.LogInformation("Remove: Item not found {0}", id);
+                return HttpNotFound();
+            }
+            ViewBag.Retry = retry ?? false;
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                Book book = await FindBookAsync(id);
+                Books.Remove(book);
+                await Books.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                return RedirectToAction("Remove", new { id = id, retry = true });
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ src/Books/Repository/Access.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Linq;
+using Microsoft.Data.Entity.Infrastructure;
+using System.Linq.Expressions;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Books.Repository
+{
+    public interface IReadOnlyRepository<out T> : IDisposable
+    {
+        T FindById(int id);
+        IQueryable<T> All();
+    }
+
+    public interface IWriteOnlyRepository<in T> : IDisposable
+    {
+        void Add(T newEntity);
+        void Remove(T entity);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+    }
+
+    public interface IRepository<T> : IReadOnlyRepository<T>, IWriteOnlyRepository<T>, IDisposable { }
+
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext _ctx;
+        protected readonly DbSet<T> _set;
+
+        public GenericRepository(DbContext ctx)
+        {
+            _ctx = ctx;
+            _set = _ctx.Set<T>();
+        }
+
+        public void Add(T newEntity)
+        {
+            _set.Add(newEntity);
+        }
+
+        public void Remove(T entity)
+        {
+            _set.Remove(entity);
+        }
+
+        public void Dispose()
+        {
+            _ctx.Dispose();
+        }
+
+        public IQueryable<T> All()
+        {
+            return _set.AsQueryable();
+        }
+
+        public T FindById(int id)
+        {
+            return _set.Find(id); //https://github.com/aspnet/EntityFramework/issues/2012
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return _ctx.SaveChangesAsync(cancellationToken);
+        }
+    }
+
+    public static class Extensions
+    {
+        public static TEntity Find<TEntity>(this DbSet<TEntity> set, params object[] keyValues) where TEntity : class
+        {
+            var context = ((IInfrastructure<IServiceProvider>)set).GetService<DbContext>();
+
+            var entityType = context.Model.FindEntityType(typeof(TEntity));
+            var key = entityType.FindPrimaryKey();
+
+            var entries = context.ChangeTracker.Entries<TEntity>();
+
+            var i = 0;
+            foreach (var property in key.Properties)
+            {
+                entries = entries.Where(e => e.Property(property.Name).CurrentValue == keyValues[i]);
+                i++;
+            }
+
+            var entry = entries.FirstOrDefault();
+            if (entry != null)
+            {
+                // Return the local object if it exists.
+                return entry.Entity;
+            }
+
+            // TODO: Build the real LINQ Expression
+            // set.Where(x => x.Id == keyValues[0]);
+            var parameter = Expression.Parameter(typeof(TEntity), "x");
+            var query = set.Where((Expression<Func<TEntity, bool>>)
+                Expression.Lambda(
+                    Expression.Equal(
+                        Expression.Property(parameter, "Id"),
+                        Expression.Constant(keyValues[0])),
+                    parameter));
+
+            // Look in the database
+            return query.FirstOrDefault();
+        }
+    }
+}
+
+
